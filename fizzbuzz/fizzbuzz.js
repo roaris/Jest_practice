e5ff28cb2302a8a5f36a2208248973568a33b1af
roaris <one0803kyopro@gmail.com>
2021-09-19 23:37:22 +0900

    テスト記述

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,8 +1,16 @@
 const toFizzBuzz = (num) => {
+  if (outofRange(num)) throw new RangeError('引数が不正です。1以上の整数のみ引数に指定可能です。');
   if (num % 15 == 0) return 'FizzBuzz';
   if (num % 3 == 0) return 'Fizz';
   if (num % 5 == 0) return 'Buzz';
   return num;
-}
+};
+
+const outofRange = (num) => {
+  if (!(typeof num == 'number')) return true;
+  if (Math.round(num) != num) return true;
+  if (num < 1) return true;
+  return false;
+};
 
 module.exports = toFizzBuzz;
