4470753a9fcde922695902cefb882cc405c2e98e
roaris <one0803kyopro@gmail.com>
2021-09-19 23:13:13 +0900

    別ファイルに切り分け

--- fizzbuzz/app.js
+++ fizzbuzz/app.js
@@ -1,14 +1,9 @@
+const toFizzBuzz = require('./fizzbuzz')
+
 const app = () => {
   for (let i = 1; i <= 100; i++) {
     console.log(toFizzBuzz(i));
   }
-}
-
-const toFizzBuzz = (num) => {
-  if (num % 15 == 0) return 'FizzBuzz';
-  if (num % 3 == 0) return 'Fizz';
-  if (num % 5 == 0) return 'Buzz';
-  return num;
 }
 
 module.exports = app;
---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,8 @@
+const toFizzBuzz = (num) => {
+  if (num % 15 == 0) return 'FizzBuzz';
+  if (num % 3 == 0) return 'Fizz';
+  if (num % 5 == 0) return 'Buzz';
+  return num;
+}
+
+module.exports = toFizzBuzz;
